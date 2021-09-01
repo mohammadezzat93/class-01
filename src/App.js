6f4e6ab57b8d7553e4c3a3ba6a2413a28fa0e53f
mohammadezzat93 <mohammadd9390@gmail.com>
2021-09-01 19:40:05 +0300

    Add Component HornedBeast

--- src/App.js
+++ src/App.js
@@ -2,58 +2,14 @@ import React from 'react';
 import Main from './component/Main'
 import Header from './component/Header'
 import Footer from './component/Footer'
-import SelectedBeast from './component/SelectedBeast';
-import data from './component/Hornedbeast.json';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row';
-// import unicorn from './image/unicorn.jpg'
 
 class App extends React.Component {
-
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      total: 15
-    }
-  }
-
-  calTotal = () =>{
-    // this.setState({
-    //   total : this.state.total - 1
-    // })
-  }
-
-  show = () =>{
-       this.setState({
-        visible : true
-    });
-    return(
-      <>
-    <SelectedBeast />
-    </>
-    )
-  }
 
   render() {
     return (
       <>
         <Header />
-        {/* <h3>total : {this.state.total}</h3> */}
-        <Row xs={1} md={3} className="g-4">
-          {data.map(item => {
-            return (
-              <Main
-                title={item.title}
-                imgUrl={item.image_url}
-                description={item.description}
-                sendMethodTotalToMain = {this.calTotal}
-                showModel = {this.show}
-                
-              />
-            )
-          })}
-        </Row>
+        <Main />
         <Footer />
       </>
 
